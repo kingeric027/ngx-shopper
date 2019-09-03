0ee2fb03e777bee5af5109d450f51e54fd85c503
Eric King <eking@four51.com>
2019-09-03 15:16:09 -0500

    finallized regex

--- src/UI/Buyer/post-build.js
+++ src/UI/Buyer/post-build.js
@@ -1,11 +1,17 @@
-const path = require('path')
 const fs = require('fs')
-const util = require('util')
-const moment = require('moment')
 
 console.log("post build running...");
+
+//need to replace certain files in 
 fs.readFile('./dist/base-buyer-server/main.js', 'utf8', (err, data) => {
     if (err) throw err;
-    var result = data.replace(/angular-tree-component\/dist\/components/g, 'angular-tree-component/dist/angular-tree-component.umd.js')
-    console.log(data);
+    fs.writeFile('./dist/base-buyer-server/main_old.js', data, 'utf8', function(err) {
+        if (err) throw err;
+    })
+    var result = data.replace(/(.\/node_modules\/)?angular-tree-component\/dist\/\w+\/\w.+\w/g, './node_modules/angular-tree-component/dist/angular-tree-component.umd.js')
+        .replace(/mobx-angular\/dist\/directives\/w.+\js/g, './node_modules/mobx-angular/dist/mobx-angular.umd.js');
+
+    fs.writeFile('./dist/base-buyer-server/main.js', result, 'utf8', function(err) {
+        if (err) throw err;
+    })
 })
